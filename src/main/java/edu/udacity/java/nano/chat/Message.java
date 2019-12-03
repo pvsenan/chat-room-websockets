4cdd13fc28a6b7fc7e5a9b4acf527eebc58c522b
Prabhendu <pvsenan@gmail.com>
2019-12-03 21:21:49 +0100

    Create Message model for chat application

--- src/main/java/edu/udacity/java/nano/chat/Message.java
+++ src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -1,8 +1,63 @@
 package edu.udacity.java.nano.chat;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * WebSocket message model
  */
 public class Message {
-    // TODO: add message model.
+    @JSONField(alternateNames = "username")
+    private String username;
+    @JSONField(alternateNames = "msg")
+    private String message;
+    private MessageType messageType;
+
+    public Message() {
+    }
+
+    public static String getJsonString(Message message, int onlineUserCount) {
+        Map<String, String> messageData = new HashMap<>();
+        messageData.put("username", message.getUsername());
+        messageData.put("msg", message.getMessage());
+        if (message.getMessageType() == MessageType.SPEAK) {
+            messageData.put("type", "SPEAK");
+        }
+        messageData.put("onlineCount", String.valueOf(onlineUserCount));
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.putAll(messageData);
+        return jsonObject.toJSONString();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public enum MessageType {
+        JOIN,
+        SPEAK,
+        LEAVE
+    }
 }
